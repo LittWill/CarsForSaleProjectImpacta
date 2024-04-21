ad096a0fe1a78ba3a4841f0c4ce3b5e26ede9023
Wilson Almeida <wilsonfmz14@gmail.com>
2024-04-21 18:52:26 -0300

    Estilo definido para desativação do anúncio

--- CarsForSaleFront/src/app/guards/auth.guard.ts
+++ CarsForSaleFront/src/app/guards/auth.guard.ts
@@ -1,12 +1,10 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/login.service';
-import { AlertService } from '../services/alert.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const authService = inject(AuthService);
-  const alertService = inject(AlertService);
 
   if (authService.usuarioEstaAutenticado()) {
     return true;
